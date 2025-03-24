f6f755a5e01435e2e19ad8821a4df55d99413f22
Pradeep Rekapalli <rekkapallipradeepg@gmail.com>
2025-03-24 16:13:20 -0400

    created a basic registration form

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
 const express = require('express')
 
-const port = 5000
+const PORT = 3000
 
 const app = express()
 
 
 
-app.use(express.static('./devfiles'))
+app.use(express.static('public'))
 
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(port , ()=> console.log(`Server is running on the port number : ${port}`));
+
+
+//  user details are stored like ids in Userdata field
+const usersData={}
+
+
+app.post('/register', (req, res) => {
+    const { username, password } = req.body
+    const id = `user_${Date.now()}`
+
+    const user = {
+        id,
+        username,
+        password
+    }
+
+    usersData[id] = user
+
+    console.log(`User is been registered and this are the details`, usersData[id])
+
+    return res.json({ id })
+
+})
+
+
+
+
+
+
+
+app.listen(PORT , ()=> console.log(`Server is running on the PORT number : ${PORT}`));
